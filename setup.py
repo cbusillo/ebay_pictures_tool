0996b06f79dfc9345892cd88a4a0c490129b4f6d
Chris Busillo <info@shinycomputers.com>
2023-08-24 01:13:06 -0400

    setup.py initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ebay_pictures_tool',
+    version='0.0.1',
+    packages=['ebay_pictures_tool'],
+    url='',
+    license='',
+    author='Chris Busillo',
+    author_email='cbusillo@gmail.com',
+    description='tool to quickly process ebay images'
+)
